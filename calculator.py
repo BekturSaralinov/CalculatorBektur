cfa15517e5c790a1b04744d80430191402851b13
BekturSaralinov <107107577+BekturSaralinov@users.noreply.github.com>
2022-06-13 19:56:32 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+first_num = float(input("Введите первое число>>"))
+oper = input("ВВедите операцию")
+second_num = float(input("Введите второе число"))
+if oper == '+':
+    print(first_num + second_num)
+elif oper == '-':
+    print (first_num - second_num)
+elif oper == '*':
+    print (first_num * second_num)
+elif oper == '/':
+    print(first_num / second_num)
+elif oper == '**':
+    print(first_num ** second_num)
+
